8bf36653c4054ba2de861a3c109b7f2dae66126f
MaratXcode <kosjachok8@yandex.ru>
2024-06-18 02:59:06 +0300

    Initial Commit

---
+++ ContactsList.V2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactsList.V2
+//
+//  Created by Marat Fakhrizhanov on 18.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
